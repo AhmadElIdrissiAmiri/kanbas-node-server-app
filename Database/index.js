a7f5f7afe900173f336ce74e36828f28920e53ad
DESKTOP-U8GBATB\ahmad <ahmadelidrissiamiri@gmail.com>
2023-11-15 19:35:22 -0500

    5 node commit

--- Database/index.js
+++ Database/index.js
@@ -1,33 +1,9 @@
-// import courses from "./courses.json" assert { type: "json" };
-// import modules from "./modules.json" assert { type: "json" };
-// import assignments from "./assignments.json" assert { type: "json" };
-// import users from "./users.json" assert { type: "json" };
-// import grades from "./grades.json" assert { type: "json" };
-// import enrollments from "./enrollments.json" assert { type: "json" };
-// export default {
-//   courses,
-//   modules,
-//   assignments,
-//   users,
-//   grades,
-//   enrollments,
-// };
-
-import assert from 'assert';
-import courses from "./courses.json";
-import modules from "./modules.json";
-import assignments from "./assignments.json";
-import users from "./users.json";
-import grades from "./grades.json";
-import enrollments from "./enrollments.json";
-
-assert.ok(courses, 'courses should be defined');
-assert.ok(modules, 'modules should be defined');
-assert.ok(assignments, 'assignments should be defined');
-assert.ok(users, 'users should be defined');
-assert.ok(grades, 'grades should be defined');
-assert.ok(enrollments, 'enrollments should be defined');
-
+import courses from "./courses.json" assert { type: "json" };
+import modules from "./modules.json" assert { type: "json" };
+import assignments from "./assignments.json" assert { type: "json" };
+import users from "./users.json" assert { type: "json" };
+import grades from "./grades.json" assert { type: "json" };
+import enrollments from "./enrollments.json" assert { type: "json" };
 export default {
   courses,
   modules,
@@ -36,3 +12,4 @@ export default {
   grades,
   enrollments,
 };
+
